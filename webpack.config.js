0e21a5de6d57c7bcba5a1bd6e64504a44a45b87c
vmwhoami <vmwhoami@gmail.com>
2021-01-04 17:37:52 +0200

    create _test_folder add jest testing framework

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,13 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 3000
+  }
+  ,
   module: {
     rules: [
       {

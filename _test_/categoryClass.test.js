ba5eee4fadc7e87d83590efbdf2cb7889a303443
vmwhoami <vmwhoami@gmail.com>
2021-01-04 22:54:45 +0200

    add the todoList test

---
+++ _test_/categoryClass.test.js
@@ -0,0 +1,21 @@
+const category = require('../src/components/categoryClass')
+describe("expect the Category to be a class", () => {
+  let cat
+  beforeEach(() => {
+    cat = new category.Category("the best thing");
+  })
+
+  it("It expects to create a category instance", () => {
+    expect(cat).toBeInstanceOf(category.Category)
+  })
+
+  it("it expect that an category should have array of items", () => {
+
+    cat.items.push("hello")
+    expect(cat.items.[0]).toBe("hello")
+  })
+
+})
+
+
+
---
+++ _test_/todoClass.test.js
@@ -0,0 +1,29 @@
+const todo = require('../src/components/todoClass')
+
+
+describe('Expect to create todoClass', () => {
+  let todos
+
+  beforeEach(() => {
+    todos = new todo.Todo("Item");
+  })
+
+  it("It is expected to create a todo Instance", () => {
+    expect(todos).toBeInstanceOf(todo.Todo)
+  })
+
+  it("It is expected to create a todo Instance", () => {
+    expect(todos.title).toBe("Item")
+  })
+
+  it("should have a checked set to false", () => {
+    expect(todos.checked).toBe(false)
+  })
+
+  it("should have an empty description", () => {
+    expect(todos.description).toBe("")
+  })
+
+
+})
+

b45e4fe9619bacdb4a2500f554f1379f7ea73ea6
vmwhoami <vmwhoami@gmail.com>
2021-01-06 13:36:36 +0200

    add the create card testing

--- _tests_/createCard.test.js
+++ _tests_/createCard.test.js
@@ -9,13 +9,32 @@ describe("Test card creation function", () => {
     expect(createCard).toBeDefined()
   })
 
+  test("createCard not to be undefinedefined", () => {
+    expect(createCard).not.toBeUndefined()
+  })
+
   test("createCard function to create a card with a title", () => {
     let title = document.querySelector('.card__h3')
     expect(title.textContent).toBe("thetest title");
   })
 
   test("createCard function to create a card with a title", () => {
+    let title = document.querySelector('.card__h3')
+    expect(title.textContent).not.toBe("another title");
+  })
+
+  test("On every invocation the function should create a card", () => {
     let title = document.querySelectorAll('.card__h3')
-    expect(title.length).toBe(3);
+    expect(title.length).not.toBe(3);
+  })
+
+  test("On every invocation the function should create a card", () => {
+    let title = document.querySelectorAll('.card__h3')
+    expect(title.length).toBe(6);
+  })
+
+  test("Expect the created card to clone svg ", () => {
+    let svg = document.querySelector('.card__delete').childNodes
+    expect(svg[0].nodeName).toBe("svg");
   })
 });
